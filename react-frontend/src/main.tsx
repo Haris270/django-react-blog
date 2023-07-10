4a998b15642bdf78d24a4e1caf7ac39e22f308d7
HarisZ <m.haris425@gmail.com>
2023-07-10 12:16:16 -0400

    create main structure

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"; //"react-dom/client";
+import App from "./App.tsx";
+import NavBar from "./components/Navbar.tsx";
+import ImageSlider from "./components/ImageSlider.tsx";
+import ContentNavbar from "./components/ContentNavbar.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./components/style.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // const routing = (
+  <Router>
+    <React.StrictMode>
+      <NavBar />
+      <ImageSlider />
+      <ContentNavbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+
+      {/* <App />  */}
+    </React.StrictMode>
+  </Router>
+);
