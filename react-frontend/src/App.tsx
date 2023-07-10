164c75e6777a31a3a9b1b754b82947cefe25114e
HarisZ <m.haris425@gmail.com>
2023-07-10 12:01:08 -0400

    create connection with api to fetch data

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+// class connectionExample extends React.Component {
+//   componentDidMount() {
+//     const apiUrl = "http://127.0.0.1:8000/api/";
+//     fetch(apiUrl)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+//   }
+//   render() {
+//     return <div>Example connection</div>;
+//   }
+// }
 
+// export default connectionExample;
 function App() {
-  const [count, setCount] = useState(0)
+  const [appState, setAppState] = useState({
+    posts: null,
+  });
+
+  useEffect(() => {
+    //   setAppState({ loading: true });
+    const apiUrl = "http://127.0.0.1:8000/api/";
+    fetch(apiUrl)
+      .then((data) => data.json())
+      .then((posts) => {
+        console.log("response from api:", posts);
+        setAppState({ posts: posts });
+      });
+  }, []);
+
+  // });
+
+  // },{setAppState});
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Heya!!</h1>
+      <Posts posts={appState.posts} />
+    </div>
+  );
+
+  // })
 }
 
-export default App
+export default App;
