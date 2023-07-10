862c969ae6d14ac8b765dc22998119a4ff2a0cfc
HarisZ <m.haris425@gmail.com>
2023-07-10 11:28:37 -0400

    create content navbar component

---
+++ react-frontend/src/components/ContentNavbar.tsx
@@ -0,0 +1,24 @@
+function ContentNavbar() {
+  return (
+    <>
+      <nav className="topic-nav">
+        <ul className="topics">
+          <a className="topic-life" href="#">
+            Life
+          </a>
+          <a className="topic-sports" href="#">
+            Sports
+          </a>
+          <a className="topic-tech" href="#">
+            Tech
+          </a>
+          <a className="topic-philosophy" href="#">
+            Philosophy
+          </a>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default ContentNavbar;
